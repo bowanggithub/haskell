3a962898310953e8b2179803ed66400429079223
Bo Wang <orchidwangbo@gmail.com>
2015-09-04 15:17:34 -0400

    Modules list

--- functions.hs
+++ functions.hs
@@ -1,3 +1,4 @@
+import Data.List
 doubleMe x = x + x
 --doubleUs x y = 2*x + 2*y
 doubleUs x y = doubleMe x + doubleMe y
@@ -164,3 +165,31 @@ quicksort (x:xs) =
     let smallerSorted = quicksort [a | a <- xs, a <= x]
         biggerSorted = quicksort [a | a <- xs, a > x]
     in smallerSorted ++ [x] ++ biggerSorted
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n 
+    | even n = n : chain (n `div` 2)
+    | odd n  = n : chain (n*3+1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
