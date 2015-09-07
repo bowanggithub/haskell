f5b77c796e5adcbe59f93742acf3dd1a9f4e7fe5
bowang <orchidwangbo@gmail.com>
2015-09-07 19:48:05 -0400

    IO

--- helloworld.hs
+++ helloworld.hs
@@ -1,5 +1,16 @@
 --main = putStrLn "hello, world"
+--main = do
+--    putStrLn "Hello, what is your name?"
+--    name <- getLine
+--    putStrLn ("Hey " ++ name ++ ", you rock!")
+
 main = do
-    putStrLn "Hello, what is your name?"
-    name <- getLine
-    putStrLn ("Hey " ++ name ++ ", you rock!")
+    line <- getLine
+    if null line
+        then return ()
+    else do
+        putStrLn $ reverseWords line
+        main
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
